6475ded23cad820c7502f3c1272cda6862d72049
Arthur L Molina <arthur.molina.dev@gmail.com>
2025-04-09 23:54:50 -0300

    criando um crud

--- Front-End-Facul/crud/script.js
+++ Front-End-Facul/crud/script.js
@@ -1,15 +1,49 @@
 let pessoas = [];
 
-function add(){
-    let nome = document.getElementById('nome');
-    let idade = document.getElementById('idade');
-    let pessoa = {
-        nome: nome.value,
-        idade: idade.value
-    };
-    pessoas.push(pessoa);
-    nome.value = '';
-    idade.value = '';
-    console.log(pessoas);
+function add() {
+    let nomePreenchido = document.getElementById('nome').value;
+    let idadePreenchida = document.getElementById('idade').value;
+    adicionarNoArray(nomePreenchido, idadePreenchida);
+    apresentarArray();
+    limparArray();
 }
 
+function adicionarNoArray(nome, idade) {
+    pessoas.push(
+        {
+            'nome': nome, 'idade': idade
+        }
+    );
+}
+
+function apresentarArray() {
+    console.log("------------ Lista de Pessoas ------------");
+
+    pessoas.forEach(function (item) {
+        console.log(`Nome: ${item.nome}  -  Idade: ${item.idade}`);
+    });
+
+    console.log("------------------------------------------");
+}
+
+function limparArray() {
+    document.getElementById('nome').value = '';
+    document.getElementById('idade').value = '';
+}
+
+function verificar() {
+    let nomePreenchido = document.getElementById('searchName').value;
+    let nomeEncontrado = false;
+    pessoas.forEach(function (item, index) {
+        if (nomePreenchido == item.nome) {
+            nomeEncontrado = true;
+        }
+    });
+
+    if (nomeEncontrado) {
+        alert("Este nome está na lista");
+    } else {
+        alert("Este nome não esta na lista");
+    }
+    limparArray()
+}
